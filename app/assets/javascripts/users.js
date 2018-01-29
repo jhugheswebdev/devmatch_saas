d0d3f202db9915d92a441a5598a824a74d9bbbf6
James Hughes <heezmagnif@gmail.com>
2018-01-29 13:02:55 -0500

    Saved to check Stripe API issues

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,34 +1,39 @@
 $(document).ready(function() {
-    Stripe.setPublishableKey($('meta[name="ststripe-key"]').attr('content'));
-    // Watch for a form submission:
-    $("#form-submit-btn").click(function(event) {
-        event.preventDefault();
-        $('input[type=submit]').prop('disable', true);
-        var error = false;
-        var ccNum = $('#card_number').val(),
-        var cvcNum = $('#card_code').val(),
-        var expMonth = $('#card_month').val(),
-        var expYear = $('#card_year').val();
-
-        if (!error) {
-            Stripe.createToken({
-                number: ccNum,
-                cvc: cvcNum,
-                exp_month: expMonth,
-                exp_year: expYear
-            }, stripeResponseHandler);
-        }
-        return false;
-    }); // form submission
-
-    function stripeResponseHandler(status, response) {
-        // Get a reference to the form:
-        var token = response.id;
-
-        // Add the token to the form:
-        f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />')
-
-        // Submit form:
-        f.get(0).submit();
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
     }
+    return false;
+  }); // form submission
+  function stripeResponseHandler(status, response) {
+    var $form = $('#payment-form');
+
+    if (response.error) {
+      // Show the errors on the form
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+    } else {
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+      $form.get(0).submit();
+    }
+  };
 });
