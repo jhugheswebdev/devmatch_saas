36c3925f96125f34fa7a1201c9a412511c02fa71
James Hughes <heezmagnif@gmail.com>
2018-01-30 13:21:09 -0500

    Adds js for send card fields to stripe

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,39 +1,28 @@
-$(document).ready(function() {
-  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  // Watch for a form submission:
-  $("#form-submit-btn").click(function(event) {
+/* global $, Stripe */
+//Document ready.
+$(document).on('turbolinks:load', function(){
+  var theForm = $('#pro_form');
+  var submitBtn = $('#form-submit-btn');
+  //Set Stripe public key.
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+  //When user clicks form submit btn,
+  submitBtn.click(function(event){
+    //prevent default submission behavior.
     event.preventDefault();
-    $('input[type=submit]').prop('disabled', true);
-    var error = false;
+    //Collect the credit card fields.
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-    if (!error) {
-      // Get the Stripe token:
-      Stripe.card.createToken({
-        number: ccNum,
-        cvc: cvcNum,
-        exp_month: expMonth,
-        exp_year: expYear
-      }, stripeResponseHandler);
-    }
-    return false;
-  }); // form submission
-  function stripeResponseHandler(status, response) {
-    var $form = $('#payment-form');
-
-    if (response.error) {
-      // Show the errors on the form
-      $form.find('.payment-errors').text(response.error.message);
-      $form.find('button').prop('disabled', false);
-    } else {
-      // response contains id and card, which contains additional card details
-      var token = response.id;
-      // Insert the token into the form so it gets submitted to the server
-      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-      // and submit
-      $form.get(0).submit();
-    }
-  };
+    //Send the card info to Stripe.
+    Stripe.createToken({
+      number: ccNum,
+      cvc: cvcNum,
+      exp_month: expMonth,
+      exp_year: expYear
+    }, stripeResponseHandler);
+  });
+  //Stripe will return a card token.
+  //Inject card token as hidden field into form.
+  //Submit form to our Rails app.
 });
