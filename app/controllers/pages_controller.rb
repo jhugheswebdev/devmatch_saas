7532665955c72a1dd98fbadd38f3cc591e47974b
James Hughes <heezmagnif@gmail.com>
2018-01-31 13:17:09 -0500

    Good changes to Stripe js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     def home
-        @basic_plan = Plan.find(1)
-        @pro_plan = Plan.find(2)
+        @basic_plan = Plan.order(1).first
+        @pro_plan = Plan.order(2).first
     end
 
     def about
